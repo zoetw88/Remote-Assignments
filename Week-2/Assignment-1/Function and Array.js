2bcfa7ceb73bf5790f9bad2115e9c1379bceeaa5
Zoe <50069243+zoetw88@users.noreply.github.com>
2020-09-20 23:10:44 +0800

    Create Function and Array.js

---
+++ Week-2/Assignment-1/Function and Array.js
@@ -0,0 +1,12 @@
+function max(numbers) {
+// your code here, for-loop method preferred
+}
+function findPosition(numbers, target) {
+// your code here, for-loop method preferred
+}
+console.log( max([1, 2, 4, 5]) ); // should print 5
+console.log( max([5, 2, 7, 1, 6]) ); // should print 7
+console.log( findPosition([5, 2, 7, 1, 6], 5) ); // should print 0
+console.log( findPosition([5, 2, 7, 1, 6], 7) ); // should print 2
+console.log( findPosition([5, 2, 7, 7, 7, 1, 6], 7) ); // should print 2 (the first position)
+console.log( findPosition([5, 2, 7, 1, 6], 8) ); // should print -1
