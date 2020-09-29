7d5f3fe19017ef8677b814bcdc65056a2bb80fde
Zoe <50069243+zoetw88@users.noreply.github.com>
2020-09-29 20:17:07 +0800

    Week3

--- Week-3/Assignment/routes/myName.js
+++ Week-3/Assignment/routes/myName.js
@@ -9,24 +9,18 @@ router.use(express.static('public'))
 
 
 
-
-router.use(express.static('public'))
-
 router.get('/', (req,res)=> {
- 
-if (!req.cookies.username){
-  console.log(req.cookies.value);
-  res.render('trackname');
-  
-}
 
+if (req.cookies.username==='undefined'||!req.cookies.username){
+  res.render('trackname');
+  console.log(req.cookies.username);
+  }
 else {
     res.render('name',{name:req.cookies.username});
-  
+    console.log('A'+req.cookies.username);
+ 
   }
-
-  
-
 });
+
 
 module.exports = router;
